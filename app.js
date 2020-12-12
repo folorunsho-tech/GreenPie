d40d24e2eb9064010503aba48ccb12a87d53a410
folorunsho-tech <64138997+folorunsho-tech@users.noreply.github.com>
2020-12-12 12:43:01 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const inner = document.querySelector(".inner");
 const recipesRapper = document.querySelector(".main-contents");
 const recipeContainer = document.querySelector(".extra");
 const detailsContainer = document.querySelector(".details-container");
+const optionSelect = document.querySelector(".select");
+const contentLoad = document.querySelector(".content-load");
 // const contentName = document.querySelector(".content-name");
 // const contentImg = document.querySelector(".content-image");
 // const contentInfo = document.querySelector(".content-description");
@@ -35,7 +37,7 @@ const renderContent = ({ strMeal, strMealThumb, idMeal }) => {
   recipesContainer.classList.add("recipe");
   recipesContainer.innerHTML = `
   <div class="extra" id="${idMeal}">
-  <i class="far fa-heart to-favorites heart"></i>
+  <!--<i class="far fa-heart to-favorites heart"></i>-->
   <img src="${strMealThumb}" alt="${strMeal}" class="recipe-img" />
   </div>
   <h4 class="recipe-name">${strMeal}</h4>
@@ -73,8 +75,46 @@ const displayContentInfo = (id) => {
 hamburgerMenu.addEventListener("click", () => {
   navMenu.classList.toggle("slide-in");
 });
-inner.addEventListener("click", () => {
+inner.addEventListener("click", (e) => {
   navMenu.classList.remove("slide-in");
+  const clickedParent = e.target.parentElement;
+  const getMeal = () => {
+    const option = e.target.innerText;
+    recipesRapper.innerHTML = "Loading";
+    contentLoad.textContent = option;
+    const fetchApi = async (link) => {
+      let attach = `c=${option}`;
+      if (!clickedParent.classList.contains("category")) {
+        attach = `a=${option}`;
+      } else attach;
+      link = `https://www.themealdb.com/api/json/v1/1/filter.php?${attach}`;
+      const response = await axios.get(link);
+      console.log(response.data.meals);
+      const data = response.data.meals;
+      for (i = 0; i <= 12; i++) {
+        const meal = data[i];
+        console.log(meal);
+        if (!(meal == undefined)) {
+          renderContent(meal);
+        }
+      }
+      // data.forEach((meal) => {
+      //   // console.log(meal.strMeal);
+      //   // console.log(meal.strMealThumb);
+      //   // console.log(meal.idMeal);
+      //   renderContent(meal);
+      // });
+      //
+      return response.data.meals;
+    };
+    fetchApi();
+    recipesRapper.innerHTML = "";
+  };
+  if (clickedParent.classList.contains("category")) {
+    getMeal();
+  } else if (clickedParent.classList.contains("area")) {
+    getMeal();
+  }
 });
 inner.addEventListener("click", (e) => {
   const clicked = e.target.parentElement;
